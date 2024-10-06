27dab739e1cdc82f5e65c7cb9e6f0d8144c7718b
Krushna Thakkar <109793947+kru2710shna@users.noreply.github.com>
2024-10-06 13:50:03 -0700

    model training added

--- src/Kidney_Image_Classifier/config/configuration.py
+++ src/Kidney_Image_Classifier/config/configuration.py
@@ -1,7 +1,7 @@
 from Kidney_Image_Classifier.constants import CONFIG_FILE_PATH,PARAMS_FILE_PATH
 from pathlib import Path
 from Kidney_Image_Classifier.utils.overlapping import read_yaml, create_directories
-from Kidney_Image_Classifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig,TrainingConfig
+from Kidney_Image_Classifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig,TrainingConfig,EvaluationConfig
 import os
 
 # class manages reading configuration files and provides configuration objects for different
@@ -72,4 +72,15 @@ class ConfigurationManager:
         )
 
         return training_config
+
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/kidney-ct-scan-image",
+            mlflow_uri="https://dagshub.com/entbappy/Kidney-Disease-Classification-MLflow-DVC.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
        
