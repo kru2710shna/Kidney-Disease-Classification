18f9be30af2ca0c501d139c0068ba9c2f0dc4753
Krushna Thakkar <109793947+kru2710shna@users.noreply.github.com>
2024-10-05 17:51:04 -0700

    base modelo added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from cnnClassifier import logger
 from Kidney_Image_Classifier.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
+from Kidney_Image_Classifier.pipeline.stage02_prepare_base_model import PrepareBaseModelTrainingPipeline
+
 
 
 
@@ -8,6 +10,18 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+     
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
--- src/Kidney_Image_Classifier/entity/config_entity.py
+++ src/Kidney_Image_Classifier/entity/config_entity.py
@@ -20,3 +20,15 @@ class DataIngestionConfig:
     
     #  directory where the downloaded zip file will be extracted after download.
     unzip_dir: Path
+
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool
+    params_weights: str
+    params_classes: int
+    
